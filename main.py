c6b4f23d902d95fef4161011218c0e6d20eeface
Anish Kelkar <anishkelkar27@gmail.com>
2026-01-30 13:45:06 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.Project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPi
 from src.Project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.Project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.Project.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.Project.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -39,6 +40,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.initiate_model_training()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
